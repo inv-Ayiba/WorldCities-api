6faa71c3a844cd1de30051ca95e9e928113ce6a2
inv-Ayiba <tariogounga@gmail.com>
2024-09-21 23:36:41 +0100

    248 I think

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -23,9 +23,15 @@ namespace WorldCities.Server.Controllers
 
         // GET: api/Cities
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<City>>> GetCities()
+        //public async Task<ActionResult<IEnumerable<City>>> GetCities(int pageIndex = 0,int pageSize = 10)
+        public async Task<ActionResult<ApiResult<City>>> GetCities(int pageIndex = 0,int pageSize = 10)
+
         {
-            return await _context.Cities.ToListAsync();
+            return await ApiResult<City>.CreateAsync(
+  _context.Cities.AsNoTracking(),
+  pageIndex,
+  pageSize
+  );
         }
 
         // GET: api/Cities/5
